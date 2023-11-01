4b4692c8d16684b87b58f083bdade886c0ff7564
TanmaySharnagat07 <sharnagattanmay@gmail.com>
2023-11-01 23:16:35 +0530

    Profile Changes

--- files/src/component/Center/ProfileSection/ProfilePage.jsx
+++ files/src/component/Center/ProfileSection/ProfilePage.jsx
@@ -1,8 +1,8 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Edit from "./Edit";
 
 const ProfilePage = () => {
-  const [edit, setedit] = useState(false) ;
+  const [edit, setedit] = useState(false);
 
   return (
     <>
@@ -25,23 +25,53 @@ const ProfilePage = () => {
           </div>
           <div className="pt-1 pl-10 flex flex-col">
             <span className="text-xl font-semibold">Username</span>
-            <span>{Math.ceil(Math.random()*10)} Posts</span>
+            <span>{Math.ceil(Math.random() * 10)} Posts</span>
           </div>
         </div>
         <div className="h-64 bg-gradient-to-r from-yellow-500 to-pink-500"></div>
-        <div className="ml-4">
+        <div className="h-48 ml-4">
           <img
             src="https://wallpapers.com/images/high/cool-profile-picture-gdhlf31a1n7pcctk.webp"
             alt="profile"
             className="object-cover rounded-full h-44 w-44 -mt-20 mb-2 border-4 border-white"
           />
-          <span className="ml-2 block text-lg font-black">Username shvxb</span>
-          <span className="ml-2 text-gray-500">@Username</span>
+          <div className="ml-[680px] mr-5 -mt-[88px] max-md:mr-5">
+            <button
+              className="bg-gradient-to-t  from-yellow-500 to-pink-500 text-white w-24 h-10 flex justify-center items-center font-semibold rounded-full"
+              onClick={() => {
+                setedit(true);
+              }}
+            >
+              Edit profile
+            </button>
+            {edit ? <Edit passvar={edit} passfunc={setedit} /> : null}
+          </div>
         </div>
-        <div className="ml-[680px] mr-5 -mt-36 max-md:mr-5">
-          <button className="bg-gradient-to-t  from-yellow-500 to-pink-500 text-white w-24 h-10 flex justify-center items-center font-semibold rounded-full"
-          onClick={()=>{setedit(true);}}>Edit profile</button>
-          {edit?<Edit passvar={edit} passfunc={setedit}/> : null}
+        <div className="ml-6">
+          <p className=" text-lg font-black">Username shvxb</p>
+          <p className=" text-gray-500">@Username</p>
+          <div className="mt-2 gap-2 flex text-gray-600">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-calendar3 mt-1"
+              viewBox="0 0 16 16"
+            >
+              <path d="M14 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM1 3.857C1 3.384 1.448 3 2 3h12c.552 0 1 .384 1 .857v10.286c0 .473-.448.857-1 .857H2c-.552 0-1-.384-1-.857V3.857z" />
+              <path d="M6.5 7a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+            </svg>
+            <span>Joined March 2021</span>
+          </div>
+          <div className="flex mt-2 gap-2 cursor-pointer">
+            <span className="hover:underline text-black">
+              22k <span className="text-gray-500">Followers</span>
+            </span>
+            <span className="hover:underline text-black">
+              100 <span className="text-gray-500">Following</span>
+            </span>
+          </div>
         </div>
       </div>
     </>
